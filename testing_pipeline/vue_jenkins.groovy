7ccbf97409339bb625add7dff4f697cee719eb12
Pranit Raj <pranitraj@Pranits-Mac-mini.local>
2023-12-20 14:09:05 +0530

    vue pipeline node2

--- testing_pipeline/vue_jenkins.groovy
+++ testing_pipeline/vue_jenkins.groovy
@@ -3,7 +3,7 @@ pipeline {
 
     tools {
         // Use NodeJS installation named "NodeJS_14" (adjust as needed)
-        NodeJS "NodeJS 20.8.0"
+        NodeJS "NodeJS"
     }
 
     environment {
